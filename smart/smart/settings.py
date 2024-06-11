2af4fad5100de441564b9543a201401ede7e33ea
Pol9lk <laa2610@gmail.com>
2024-06-11 21:06:15 +0300

    2

--- smart/smart/settings.py
+++ smart/smart/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'smart.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DenisShirokov$default',
-        'USER': 'DenisShirokov',
-        'PASSWORD': 'ccj$/4_%z2q6RUS',
-        'HOST': 'DenisShirokov.mysql.pythonanywhere-services.com',
+        'NAME': 'ymnii$default',
+        'USER': 'ymnii',
+        'PASSWORD': 'Y@bu59.?u%=Uhri',
+        'HOST': 'ymnii.mysql.pythonanywhere-services.com',
         'OPTIONS': {
             'init_command': "SET NAMES 'utf8mb4';SET sql_mode = 'STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
